eb153f8aa97e0fa74f36725355884255d6bbcd71
NicoleMenorias <menorias.nicole@gmail.com>
2025-02-26 21:10:42 +0800

    Added category_id and supplier_id to Product schema

--- product-inventory-api/models/Products.js
+++ product-inventory-api/models/Products.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true, default: 0 },
+    name: { type: String, required: true},
+    description: { type: String},
+    price: { type: Number, required: true},
+    quantity: { type: Number, required: true, default: 0},
+    category_id: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
+    supplier_id: { type: mongoose.Schema.Types.ObjectId, ref: "Supplier", required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
