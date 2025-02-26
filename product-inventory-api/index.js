716a9d9576def4b007ba0ac935b54267f2fc7b5f
keithlaspona <laspona.keith@gmail.com>
2025-02-26 20:11:54 +0800

    Added Supplier Code

--- product-inventory-api/index.js
+++ product-inventory-api/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
+
 const productRoutes = require("./routes/productRoutes");
+const supplierRoutes = require("./routes/supplierRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,6 +13,7 @@ app.use(express.json()); // Middleware for JSON parsing
 
 // Routes
 app.use("/api/products", productRoutes);
+app.use("/api/suppliers", supplierRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
