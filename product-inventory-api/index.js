0eb7e5354729e1599b5231d5a05154e1666efe41
keithlaspona <laspona.keith@gmail.com>
2025-02-27 16:55:08 +0800

    Inital Commit

--- product-inventory-api/index.js
+++ product-inventory-api/index.js
@@ -21,5 +21,5 @@ app.use("/api/categories", categoryRoutes);
 app.use("/api/warehouses", warehouseRoutes);
 app.use("/api/inventory", inventoryRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
