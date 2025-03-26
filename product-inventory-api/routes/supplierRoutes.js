1069724d278501379f6c825951507b2b3121b73f
NicoleMenorias <menorias.nicole@gmail.com>
2025-03-26 15:17:36 +0800

    added new functions

--- product-inventory-api/routes/supplierRoutes.js
+++ product-inventory-api/routes/supplierRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { 
     getSuppliers, 
     getSupplierById, 
+    getSupplierByProduct, 
     createSupplier, 
     updateSupplier, 
     deleteSupplier 
@@ -12,6 +13,7 @@ const router = express.Router();
 // Routes
 router.get("/", getSuppliers); // Get all suppliers
 router.get("/:id", getSupplierById); // Get a single supplier by ID
+router.get("/product/:product_id", getSupplierByProduct); // Get supplier by product ID
 router.post("/", createSupplier); // Add a new supplier
 router.put("/:id", updateSupplier); // Update supplier by ID
 router.delete("/:id", deleteSupplier); // Delete supplier by ID
