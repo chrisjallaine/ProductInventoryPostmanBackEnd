5f3b0886e11069ec7d95ffb66d626d607942a8cd
NicoleMenorias <menorias.nicole@gmail.com>
2025-03-26 14:56:17 +0800

    added reference

--- product-inventory-api/models/Category.js
+++ product-inventory-api/models/Category.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true, trim: true },
-    description: { type: String, trim: true },
-    createdAt: { type: Date, default: Date.now }
-});
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true, unique: true },
+  },
+  { timestamps: true } // Auto-generates createdAt & updatedAt fields
+);
 
 module.exports = mongoose.model("Category", categorySchema);
-
-// changed
