581ccb43947ccb56e62988f6747933032f292ec0
NicoleMenorias <menorias.nicole@gmail.com>
2025-03-26 14:55:16 +0800

    added reference

--- product-inventory-api/models/Inventory.js
+++ product-inventory-api/models/Inventory.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const inventorySchema = new mongoose.Schema({
+const inventorySchema = new mongoose.Schema(
+  {
     product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
     warehouse_id: { type: mongoose.Schema.Types.ObjectId, ref: "Warehouse", required: true },
-    stock_quantity: { type: Number, required: true, default: 0 },
-    lastUpdated: { type: Date, default: Date.now },
-});
+    stock: { type: Number, required: true, min: 0 },
+  },
+  { timestamps: { createdAt: false, updatedAt: "lastUpdated" } } // Only stores lastUpdated timestamp
+);
 
 module.exports = mongoose.model("Inventory", inventorySchema);
