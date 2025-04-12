4e0e77d058d698b71a5595a62a729a1e52edfee5
Nicole Menorias <162808644+NicoleMenorias@users.noreply.github.com>
2025-04-13 04:11:46 +0800

    Update supplierRoutes.js

--- product-inventory-api/routes/supplierRoutes.js
+++ product-inventory-api/routes/supplierRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const supplierController = require('../controllers/supplierController');
+const express = require("express");
+const {
+  recordSupplierDelivery
+} = require("../controllers/supplierController");
 
-router.post('/', supplierController.createSupplier);
-router.get('/', supplierController.getAllSuppliers);
-router.get('/:id/products', supplierController.getSupplierWithProducts);
+const router = express.Router();
+
+router.put("/:supplierId/delivery", recordSupplierDelivery);
 
 module.exports = router;
