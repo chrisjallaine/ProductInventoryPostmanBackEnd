2dbc4a2c00a234e67e89919ab56b923508cc1d1a
Nicole Menorias <162808644+NicoleMenorias@users.noreply.github.com>
2025-04-13 04:10:14 +0800

    Update productRoutes.js

--- product-inventory-api/routes/productRoutes.js
+++ product-inventory-api/routes/productRoutes.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
+const express = require("express");
+const {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  getProductBySKU
+} = require("../controllers/productController");
 
-router.post('/', productController.createProduct);
-router.get('/', productController.getAllProducts);
-router.get('/quantities/summary', productController.getProductsWithQuantity);
+const router = express.Router();
+
+router.post("/", createProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.get("/sku/:sku", getProductBySKU);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
