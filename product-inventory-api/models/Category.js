2f5c289820314c5a6518c9ec9f39eac15ec48718
Nicole Menorias <162808644+NicoleMenorias@users.noreply.github.com>
2025-04-12 16:49:00 +0800

    Update Category.js

--- product-inventory-api/models/Category.js
+++ product-inventory-api/models/Category.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true, trim: true, unique: true },
-  },
-  { timestamps: true } // Auto-generates createdAt & updatedAt fields
-);
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }
+}, { timestamps: true });
 
-module.exports = mongoose.model("Category", categorySchema);
+module.exports = mongoose.model('Category', categorySchema);
