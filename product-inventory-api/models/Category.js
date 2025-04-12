0a66152d4aeb2e503a9e1751bd2732ffea6ff8f1
Nicole Menorias <162808644+NicoleMenorias@users.noreply.github.com>
2025-04-13 04:04:34 +0800

    Update Category.js

--- product-inventory-api/models/Category.js
+++ product-inventory-api/models/Category.js
@@ -1,12 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    }
+  name: { type: String, required: true, unique: true },
+  productCount: { type: Number, default: 0 }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
