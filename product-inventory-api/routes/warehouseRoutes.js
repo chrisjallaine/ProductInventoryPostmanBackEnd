cd10c50413429056833fecc817380dd07a815621
Nicole Menorias <162808644+NicoleMenorias@users.noreply.github.com>
2025-04-13 04:10:58 +0800

    Update warehouseRoutes.js

--- product-inventory-api/routes/warehouseRoutes.js
+++ product-inventory-api/routes/warehouseRoutes.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const warehouseController = require('../controllers/warehouseController');
+const express = require("express");
+const {
+  getWarehouseById,
+  getUtilization
+} = require("../controllers/warehouseController");
 
-router.post('/', warehouseController.createWarehouse);
-router.get('/', warehouseController.getAllWarehouses);
-router.get('/:id/stock', warehouseController.getWarehouseStock);
+const router = express.Router();
+
+router.get("/:id", getWarehouseById);
+router.get("/:id/utilization", getUtilization);
 
 module.exports = router;
