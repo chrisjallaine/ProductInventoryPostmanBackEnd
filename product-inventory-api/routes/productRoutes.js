f4aaf5690947be65e99fb7a6712f0119d70893b8
NicoleMenorias <menorias.nicole@gmail.com>
2025-04-14 22:19:14 +0800

    updated

--- product-inventory-api/routes/productRoutes.js
+++ product-inventory-api/routes/productRoutes.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const {
   createProduct,
-  getProducts,
+  getAllProducts,
   getProductById,
+  getProductBySKU,
   updateProduct,
   deleteProduct,
-  getProductBySKU
+  getProductsByWarehouse,
+  getProductsByCategory,
+  getProductsBySupplier
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.post("/", createProduct);
-router.get("/", getProducts);
+router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 router.get("/sku/:sku", getProductBySKU);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
+
+// Relational Queries
+router.get("/warehouse/:warehouseId", getProductsByWarehouse);
+router.get("/category/:categoryId", getProductsByCategory);
+router.get("/supplier/:supplierId", getProductsBySupplier);
 
 module.exports = router;
